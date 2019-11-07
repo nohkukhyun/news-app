e66ddc65588d8057a25afaf99176dd0751b75328
nohkukhyun <bbi9l@naver.com>
2019-11-07 23:00:12 +0900

    업데이트중

--- src/shared/reducers/news.reducer.js
+++ src/shared/reducers/news.reducer.js
@@ -1,1 +1,26 @@
-import { ajax } from "rxjs";
+import { Observable, observable } from "rxjs";
+import * as action from "../actions/news.action";
+
+const initalState = {
+  payload: []
+};
+
+const fetchNews = (state = initalState, action) => {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case action.FETCH_NEWS:
+      return {
+        ...state
+      };
+
+    case action.FETCH_NEWS_SUCCES:
+      return {
+        ...state
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default fetchNews;
