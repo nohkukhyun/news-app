4beca89170e5916375f1e45694d5a0dcdce06cd2
노국현 <YNWA@nogughyeon-ui-MacBook-Pro.local>
2019-11-17 23:06:18 +0900

    11/17

---
+++ src/routes/news/business.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const BusinessWrap = styled.div`
+  position: relative;
+`;
+
+const business = () => {
+  return <div></div>;
+};
+
+export default business;
